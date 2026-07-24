c4bba20cd466d870f25b10724a7db5362ed1afc1
djc1416 <dj.c.1416@gmail.com>
2026-07-23 21:41:44 -0400

    Integrate AI service with OpenRouter

--- modules/ai_client.py
+++ modules/ai_client.py
@@ -1,2 +1,27 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("OPENROUTER_API_KEY"),
+    base_url="https://openrouter.ai/api/v1"
+)
+
 def ask_ai(prompt):
-    return "AI integration comming soon..."
+    response = client.chat.completions.create(
+        model="deepseek/deepseek-chat-v3-0324:free",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are grammar correction engine. Always follow the user's instructions exactly. Never provide explanations or multiple options unless explicity requeseted."
+            },
+            {
+                "role": "user",
+                "content": prompt
+            }
+
+        ]
+    )    
+    return response.choices[0].message.content
--- modules/grammar.py
+++ modules/grammar.py
@@ -1,5 +1,23 @@
 from modules.ai_client import ask_ai
 
 def correct_email(email):
-    prompt = f"Correct this email:\n\n{email}"
+    prompt = f"""
+You are an English grammar correction engine. 
+
+You only task is to rewrite the email with correct grammar, spelling, punctuation, and clarity.
+
+STRICT RULES:
+Return ONLY the corrected email.
+Do NOT explain your changes.
+Do NOT provide multiple versions.
+Do NOT add a subject line.
+Do NOT greetings or signatures that were not in the original email.
+Do NOT give advice.
+Do NOT use bullet points
+Do NOT use markdown.
+Preserve the original meaning.
+
+Email:
+{email}
+"""
     return ask_ai(prompt)
